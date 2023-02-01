778d58981e437eb43e3fb6fe7ac941f8c3363800
benedikt-lutze <116339281+benedikt-lutze@users.noreply.github.com>
2023-02-01 18:17:38 +0100

    Update immo_spider.py

--- immo_spider.py
+++ immo_spider.py
@@ -4,8 +4,7 @@ class QuotesSpider(scrapy.Spider):
     name = "immoscout"
     start_urls = [
             #'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Fahrzeitsuche/Stuttgart/70569/-64516/2093406/-/-/45/2,00-/-/EURO--850,00'
-            'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Umkreissuche/Stuttgart/70569/-64516/2093406/-/-/5/1,50-/-/EURO--850,00',
-            'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Fahrzeitsuche/Stuttgart/70569/-64516/2093406/-/-/30/2,00-/-/EURO--850,00'
+            'https://www.immobilienscout24.de/Suche/de/berlin/berlin/pankow/prenzlauer-berg/wohnung-mieten?numberofrooms=2.0-&price=-1300.0&livingspace=65.0-&pricetype=rentpermonth&sorting=2'
             ]
     def parse(self, response):
         for quote in response.css('a.result-list-entry__brand-title-container::attr(href)').extract():
